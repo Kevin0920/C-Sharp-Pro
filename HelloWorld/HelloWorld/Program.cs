f0fec505b0726b19252d0f3b51db5c2279e9c3bf
Kevin Ke <kevinke@MacBook-Pro-1023.local>
2018-03-25 22:08:42 -0700

    first commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(Add(21, 23));
+
+            Console.WriteLine(Multi(100, 2));
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Multi(int c , int d)
+        {
+            return c * d;
+        }
+    }
+}
